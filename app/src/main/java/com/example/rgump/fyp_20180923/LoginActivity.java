c777429f3fdeaf9eac9660c164f83a652a4c5287
rgump6666 <rgump6666@gmail.com>
2018-09-26 01:19:01 +0800

    20180925-1

--- app/src/main/java/com/example/rgump/fyp_20180923/LoginActivity.java
+++ app/src/main/java/com/example/rgump/fyp_20180923/LoginActivity.java
@@ -2,14 +2,17 @@ package com.example.rgump.fyp_20180923;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.rgump.fyp_20180923.include.Loader;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Loader loader = new Loader();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        System.out.print("dsafa");
     }
 }
---
+++ app/src/main/java/com/example/rgump/fyp_20180923/include/Loader.java
@@ -0,0 +1,8 @@
+package com.example.rgump.fyp_20180923.include;
+
+public class Loader {
+
+    public Loader(){
+
+    }
+}
